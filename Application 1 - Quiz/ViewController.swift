d1f15e10cc559e817a1c7998e40dde06ed3b0ab0
cwvilain <cwvilain@gmail.com>
2016-02-13 10:04:15 -0500

    Initial Commit

---
+++ Application 1 - Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Application 1 - Quiz
+//
+//  Created by Chloe W Vilain on 2/13/16.
+//  Copyright © 2016 Chloe W Vilain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
