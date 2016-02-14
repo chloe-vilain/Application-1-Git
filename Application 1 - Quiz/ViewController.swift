8dd285070f6fffa930bb931acd015eba51b1f37a
cwvilain <cwvilain@gmail.com>
2016-02-13 22:54:54 -0500

    Application v1, as described in book. No icon, no opening image

--- Application 1 - Quiz/ViewController.swift
+++ Application 1 - Quiz/ViewController.swift
@@ -9,15 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["What is your name?",
+                                "What is your quest?",
+                                "What is your favourite colour?"]
+    let answers: [String] = ["Chloe",
+                                "To learn iOS dev",
+                                "Turquoise"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+                currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+        
+    }
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        questionLabel.text = questions[currentQuestionIndex]
     }
 
 
